0bb4f3ba8d732d5cefefd3245af3703e3b1126ff
Naitik Tagline <naitikb.tagline@gmail.com>
2023-07-31 15:59:11 +0530

    Use navigation in 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
   const location = useLocation();
-  console.log(location);
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate("/");
+  }
 
   return (
     <div className="custom-container container my-5 p-5 bg-light">
       <h1> :( </h1>
       <br />
       <h3>
-        A 404 error occured, Page not found, check the URL{" "}
+        A 404 error occured, Page not found, check the Path{" "}
         <span className="text-danger"> {location.pathname} </span> and try
         again.
       </h3>
       <br />
-      <button className="btn btn-danger px-3 pt-2 ">Home</button>
+      <button onClick={handleClick} className="btn btn-danger px-3 pt-2 ">
+        Home
+      </button>
     </div>
   );
 };
