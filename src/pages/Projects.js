9047a9e7d9eaf4ba76e06aa9ed71d83c6bc8ce66
Naitik Tagline <naitikb.tagline@gmail.com>
2023-08-01 17:29:04 +0530

    Button issue fix in projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,36 +7,39 @@ import { Outlet } from "react-router";
 import { useState } from "react";
 
 const Projects = () => {
+  let arr = ["design", "development", "database"];
   let navigate = useNavigate();
   let [count, setCount] = useState(0);
-  let arr = ["design", "development", "database"];
   let arrLength = arr.length - 1;
   function handleClick() {
     setCount(count + 1);
     navigate(`${arr[count]}`);
     if (count > arrLength) {
       navigate("/projects");
-      setCount(count - arr.length);
+      setCount(count - arrLength - 1);
     }
   } // next navigate
 
   function handleClickBack() {
-    if (count > arrLength) {
-      setCount(count - arrLength);
-    } else {
-      setCount(count - 1);
-    }
+    setCount(count - 1);
     navigate(-1);
-  } // previous navigate
-
+  }
+  // previous navigate
   return (
     <Container className="my-5 text-center custom-container">
       <Heading heading="Pro" subhead="j" subheading="ects" />
       <Outlet />
       <div className="container-fluid m-5">
-        <button onClick={handleClickBack} className="btn btn-danger">
-          Back
-        </button>
+        {count ? (
+          <button onClick={handleClickBack} className="btn btn-danger ">
+            Back
+          </button>
+        ) : (
+          <button onClick={handleClickBack} className="btn btn-danger disabled">
+            Back
+          </button>
+        )}
+        {/* back button disabled when it is on projects page , {count = 0} */}
         <span> </span>
         <button onClick={handleClick} className="btn btn-primary">
           Next
