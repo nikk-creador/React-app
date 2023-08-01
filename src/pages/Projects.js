5a738921cf99bd6062d91e733aa10648952b9695
Naitik Tagline <naitikb.tagline@gmail.com>
2023-08-01 16:38:39 +0530

    Implement Back and next button in projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,29 +3,39 @@ import { Container } from "react-bootstrap";
 import "../App.css";
 import Heading from "../components/Heading";
 import { useNavigate } from "react-router-dom";
-import { useLocation, Outlet } from "react-router";
+import { Outlet } from "react-router";
+import { useState } from "react";
+
 const Projects = () => {
-  let location = useLocation();
-
   let navigate = useNavigate();
-
-  console.log(location, "path name");
+  let [count, setCount] = useState(0);
   let arr = ["design", "development", "database"];
-
+  let arrLength = arr.length - 1;
   function handleClick() {
-    navigate(`${location.pathname}/${arr[0]}`);
+    setCount(count + 1);
+    navigate(`${arr[count]}`);
+    if (count > arrLength) {
+      navigate("/projects");
+      setCount(count - arr.length);
+    }
   } // next navigate
 
   function handleClickBack() {
-    navigate(1);
+    if (count > arrLength) {
+      setCount(count - arrLength);
+    } else {
+      setCount(count - 1);
+    }
+    navigate(-1);
   } // previous navigate
+
   return (
     <Container className="my-5 text-center custom-container">
       <Heading heading="Pro" subhead="j" subheading="ects" />
       <Outlet />
       <div className="container-fluid m-5">
         <button onClick={handleClickBack} className="btn btn-danger">
-          Previous
+          Back
         </button>
         <span> </span>
         <button onClick={handleClick} className="btn btn-primary">
